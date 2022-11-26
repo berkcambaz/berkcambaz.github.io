db524d0dfbec716a63e209a400db49f880456c15
Berk Cambaz <berkcambaz12321@gmail.com>
2022-11-27 00:43:18 +0300

    Update LaL

---
+++ assets/NotFound.2a279368.js
@@ -0,0 +1,1 @@
+import{a as n,F as o}from"./index.d4fe14c2.js";function r(){return n(o,{children:"not found"})}export{r as default};
--- assets/NotFound.54f81257.js
+++
@@ -1,1 +0,0 @@
-import{a as n,F as o}from"./index.ec832975.js";function r(){return n(o,{children:"not found"})}export{r as default};
