7ce8eda3924a7395c4c4ed0a47509a612b059aca
Berk Cambaz <berkcambaz12321@gmail.com>
2023-04-23 17:18:15 +0300

    Update

---
+++ assets/NotFound-20bd7ddf.js
@@ -0,0 +1,1 @@
+import{d as n,F as o}from"./index-27357618.js";function r(){return n(o,{children:"not found"})}export{r as default};
--- assets/NotFound-b3fd3ea5.js
+++
@@ -1,1 +0,0 @@
-import{d as n,F as o}from"./index-986eea72.js";function r(){return n(o,{children:"not found"})}export{r as default};
