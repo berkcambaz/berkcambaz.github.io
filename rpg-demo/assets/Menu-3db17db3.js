a0c9802701004bbddec1aa9b2cafc8f3458aaf84
Berk Cambaz <berkcambaz12321@gmail.com>
2023-09-08 00:37:08 +0300

    Update rpg-demo

--- rpg-demo/assets/Menu-3db17db3.js
+++
@@ -1,1 +0,0 @@
-import{j as e,F as t,k as r,n as s}from"./index-350f19dc.js";function a(){return e.jsx(t,{direction:"column",gap:"md",mx:"md",my:80,children:e.jsx(r,{onClick:()=>s.getState().reset(),variant:"default",children:"Reset Progress"})})}export{a as default};
