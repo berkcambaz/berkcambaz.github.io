148de6b75d2927cdfd5999040966c93692aafd66
Berk Cambaz <berkcambaz12321@gmail.com>
2023-10-22 12:52:48 +0300

    Update crewlands-demo

---
+++ crewlands-demo/assets/NotFound-133e1932.js
@@ -0,0 +1,1 @@
+import{D as t}from"./index-61b00879.js";function n(){return t.jsx(t.Fragment,{children:"NotFound"})}export{n as default};
--- crewlands-demo/assets/NotFound-e0018a58.js
+++
@@ -1,1 +0,0 @@
-import{D as t}from"./index-e40795e7.js";function n(){return t.jsx(t.Fragment,{children:"NotFound"})}export{n as default};
