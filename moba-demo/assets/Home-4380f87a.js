3aa6cf0102c7c42b296a7256cc98aa29e868000f
Berk Cambaz <berkcambaz12321@gmail.com>
2023-11-24 00:09:04 +0300

    Add moba-demo

---
+++ moba-demo/assets/Home-4380f87a.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-99b77826.js";function t(){return e.jsx(e.Fragment,{children:"Home"})}export{t as default};
---
+++ moba-demo/assets/NotFound-e3a75c61.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-99b77826.js";function n(){return t.jsx(t.Fragment,{children:"NotFound"})}export{n as default};
