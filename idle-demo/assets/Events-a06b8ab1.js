6e1dbd9f3d4efa394541de79cef94673692c6351
Berk Cambaz <berkcambaz12321@gmail.com>
2023-12-09 15:10:50 +0300

    Update

---
+++ idle-demo/assets/Events-a06b8ab1.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-76a54e34.js";function n(){return t.jsx(t.Fragment,{children:"Events"})}export{n as default};
---
+++ idle-demo/assets/Guild-37e12fef.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-76a54e34.js";function e(){return r.jsx(r.Fragment,{children:"Guild"})}export{e as default};
