efcb3d93c22a2c062ffc53978fe97158e28c4e74
Berk Cambaz <berkcambaz12321@gmail.com>
2024-05-15 23:42:27 +0300

    Update lord-masters

--- lord-masters/assets/NotFoundPage-CGattycx.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-N9mt28CQ.js";function e(){return t.jsx(t.Fragment,{children:"NotFoundPage"})}export{e as default};
---
+++ lord-masters/assets/NotFoundPage-Cp74GQ8z.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BlLJkOLg.js";function e(){return t.jsx(t.Fragment,{children:"NotFoundPage"})}export{e as default};
